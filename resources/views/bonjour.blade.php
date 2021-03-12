c8db377e3d38f42a385f6127cedbca1bc12d5c9d
ronid-dev <mbomrobertille@gmail.com>
2021-03-12 16:05:11 +0100

    initial commit

---
+++ resources/views/bonjour.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>bonjour</title>
+        <meta charset= "utf-8">
+       
+</head>
+<style>
+.haut{font-size:15px;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+   height: 60px;
+   width: 1350px;
+   background-color: hotpink;}
+
+   .bas{font-size: 10px;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+   height: 200px;
+   width: 1000px;
+   background-color: hotpink;}
+h1{color: #037a74;
+    margin-right: 0cm;}
+    .tete2{font-size: 24%;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    margin-left:800px;
+    margin-top: -55px;
+    text-align: center;
+    height: 25px;
+width: 95px;
+background-color:#bacecc;
+}
+
+.tete3{font-size: 24%;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    margin-left:900px;
+    margin-top: -31px;
+    height: 25px;
+width: 95px;
+text-align: center;
+background-color:#bacecc;
+}
+.tete4{font-size: 24%;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    margin-left:1000px;
+    margin-top: -31px;
+    height: 25px;
+width: 95px;
+text-align: center;
+background-color:#bacecc;
+}
+.tete5{font-size: 24%;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    margin-left:1100px;
+    margin-top: -31px;
+    height: 25px;
+width: 95px;
+text-align: center;
+background-color:#bacecc;
+}
+.tete6{font-size: 24%;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    margin-left:1200px;
+    margin-top: -31px;
+    height: 25px;
+width: 95px;
+text-align: center;
+background-color:#bacecc;
+}
+.t1{font-family:impact;
+    color: black;
+    margin-top: ;
+    text-align: center;
+    }
+</style>
+<body>
+    <div class="haut">
+    <h1>TrocBOOk </h1>
+    <div class="tete2">
+    <h2 class=t1>Acceuil</h2>
+</div>
+<div class="tete3">
+    <h2 class=t1>ma vitrine </h2>
+</div>
+<div class="tete4">
+    <h2 class=t1>troc</h2>
+</div>
+<div class="tete5">
+    <h2 class= t1>mon compte</h2>
+</div>
+<div class="tete6">
+    <h2 class=t1>A-propos</h2>
+</div>
+</div>
+ 
+ <blockquote class="bas">roni ngounou copyright@2021</blockquote>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/bonjour', function () {
+
+    return view('bonjour');
+});
+
+Route::get('/acceuill', function () {
+
+    return view('acceuill');
+});
+Route::get('/inscription', function () {
+
+    return view('inscription');
+});
+Route::get('/connexion', function () {
+
+    return view('connexion');
+});
